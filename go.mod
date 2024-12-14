2411faa8890ce9e5e8d53f573794e4b64d608628
Daffa Randika <daffa.randika.3@gmail.com>
2024-12-14 15:38:38 +0700

    chore: format document

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tcp
 
 go 1.22.2
+
+require github.com/pebbe/zmq4 v1.2.11
--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 	"os"
 	s "tcp/pkg/server"
-	"encoding/json"
+	"github.com/pebbe/zmq4"
 )
 
 func main() {
 	fmt.Println("Starting the server...")
+
 	host := "0.0.0.0"
 	port := "6969"
 
@@ -25,6 +27,20 @@ func main() {
 
 	fmt.Printf("Server is listening on %s:%s\n", host, port)
 
+	// zmq stuff
+
+	publisher, err_create_sock := zmq4.NewSocket(zmq4.PUB)
+	if err_create_sock != nil {
+		fmt.Println("Error while creating ZMQ socket", err)
+		return
+	}
+	defer publisher.Close()
+
+	err_create_pub := publisher.Bind("tcp://localhost:6970")
+	if err_create_pub != nil {
+		fmt.Println("Error while creating publisher", err)
+	}
+
 	for {
 		err := server.OpenConnection()
 		if err != nil {
@@ -33,11 +49,11 @@ func main() {
 		}
 
 		fmt.Println("New client connected")
-		go handleClient(server.Connection)
+		go handleClient(server.Connection, publisher)
 	}
 }
 
-func handleClient(conn net.Conn) {
+func handleClient(conn net.Conn, publisher *zmq4.Socket) {
 	defer conn.Close()
 	fmt.Printf("Handling connection from %s\n", conn.RemoteAddr())
 
@@ -54,13 +70,18 @@ func handleClient(conn net.Conn) {
 		msg := string(buffer[:length])
 
 		var data interface{}
-        json.Unmarshal([]byte(msg), &data)
+		json.Unmarshal([]byte(msg), &data)
 
-        prettyJSON, err := json.MarshalIndent(data, "", "  ")
+		prettyJSON, err := json.MarshalIndent(data, "", "  ")
 
 		if err != nil {
 			fmt.Println("Error marshalling JSON:", err)
 			return
+		}
+
+		data, err_send := publisher.Send(msg, zmq4.DONTWAIT)
+		if err_send != nil {
+			fmt.Println("Error when sending", err_send)
 		}
 
 		fmt.Printf("RECV from %s:\n%s\n", conn.RemoteAddr(), prettyJSON)
